8630e77c9637cae6d13503a0e4f8197b46782ac2
Zachary Rizzuto <zachrizzuto@gmail.com>
2023-07-27 17:39:32 -0500

    Complete assignment

--- src/ClassTrafficLight.tsx
+++ src/ClassTrafficLight.tsx
@@ -1,17 +1,30 @@
-import { Component } from "react";
+import { Component } from "react"
+
 
 export class ClassTrafficLight extends Component {
+  state = {
+    light: 0,
+  };
+
+  increment = () => {
+    if(this.state.light + 1 > 2) {
+      this.setState({light: this.state.light = 0})
+    } else {
+      this.setState({light: this.state.light + 1})
+    }
+  }
+
   render() {
     return (
       <div className="traffic-light-box">
         <h2>Class Traffic Light</h2>
         <div className="traffic-light">
           {/* Background color can be black | yellow | red | green */}
-          <div className="circle red"></div>
-          <div className="circle black"></div>
-          <div className="circle black"></div>
+          <div className={'circle ' + (this.state.light === 0 ? 'red' : 'black')}></div>
+          <div className={'circle ' + (this.state.light === 1 ? 'yellow' : 'black')}></div>
+          <div className={'circle ' + (this.state.light === 2 ? 'green' : 'black')}></div>
         </div>
-        <button className="next-state-button">Next State</button>
+        <button className="next-state-button" onClick={this.increment}>Next State</button>
       </div>
     );
   }
